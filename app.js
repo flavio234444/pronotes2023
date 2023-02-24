508146636e5fa4b8742a1a943d4be1eba5409339
JoseLara55 <106717577+JoseLara55@users.noreply.github.com>
2023-02-24 14:53:48 +0000

    .

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 
+
+// We are creating the express instance
 var app = express();
 
 // view engine setup
+// We are declaring the localization of the views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
