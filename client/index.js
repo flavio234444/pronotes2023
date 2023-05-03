b00a9c7f37396a6bfd0308a217475a2eb305cb5c
JoseLara55 <106717577+JoseLara55@users.noreply.github.com>
2023-05-03 04:25:49 +0000

    👀Cambios con Materialize

--- client/index.js
+++ client/index.js
@@ -2,4 +2,10 @@
 /* eslint-disable no-console */
 import './styles/style.css';
 
-console.log('🎉 Estilos cargados correctamente 🎉');
+import 'materialize-css/dist/css/materialize.css';
+
+import 'materialize-css/dist/js/materialize';
+
+M.AutoInit();
+
+console.log('Webpack Working Middleware!!!! 🐱‍👤');
